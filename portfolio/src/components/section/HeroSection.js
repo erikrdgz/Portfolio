aef98728755002687dd97aa9ba1cb2d4f7a1860f
Erik Rodriguez <erikrdgz@hotmail.com>
2025-02-07 21:11:28 -0700

    animation fix

--- portfolio/src/components/section/HeroSection.js
+++ portfolio/src/components/section/HeroSection.js
@@ -35,6 +35,8 @@ const HeroSection = () => {
                   initial={{ clipPath: "inset(0 100% 0 0)" }}
                   animate={{ clipPath: "inset(0 0 0 0)" }}
                   transition={{ duration: 1, ease: "easeOut" }}
+                  style={{ willChange: "clip-path" }}
+                  
                 >
                   Product
                 </motion.span>
@@ -43,6 +45,7 @@ const HeroSection = () => {
                   initial={{ clipPath: "inset(0 100% 0 0)" }}
                   animate={{ clipPath: "inset(0 0 0 0)" }}
                   transition={{ duration: 1, delay: 0.5, ease: "easeOut" }}
+                  style={{ willChange: "clip-path" }}
                   className="text-orange-500"
                 >
                   Designer

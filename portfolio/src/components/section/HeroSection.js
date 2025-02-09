1f7bb735b2a7dcb433257fb47e17a11df81a8c0d
Erik Rodriguez <erikrdgz@hotmail.com>
2025-02-08 17:15:12 -0700

    testing animation hero

--- portfolio/src/components/section/HeroSection.js
+++ portfolio/src/components/section/HeroSection.js
@@ -21,10 +21,10 @@ const HeroSection = () => {
           <div className="flex md:justify-center items-center md:items-end">
             <motion.div
               className="flex items-start md:items-end justify-center md:justify-start pb-9 md:pb-0"
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.7, delay: 1 }}
-              layout
+              // initial={{ opacity: 0, y: 20 }}
+              // animate={{ opacity: 1, y: 0 }}
+              // transition={{ duration: 0.7, delay: 1 }}
+              // layout
             >
               <h1
                 className={`${isDarkMode ? "text-white" : "text-black"} font-bold text-6xl md:text-7xl xl:text-9xl`}
@@ -50,11 +50,11 @@ const HeroSection = () => {
             <div className="w-full md:w-10/12">
               <motion.p
                 className={`${isDarkMode ? "text-white" : "text-black"} md:text-left text-lg md:text-md lg:text-md xl:text-xl`}
-                initial={{ opacity: 0, y:  -20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.7, delay: 1.2 }}
-                style={{ lineHeight: "1.8" }}
-                layout
+                // initial={{ opacity: 0, y:  -20 }}
+                // animate={{ opacity: 1, y: 0 }}
+                // transition={{ duration: 0.7, delay: 1.2 }}
+                // style={{ lineHeight: "1.8" }}
+                // layout
               >
                 Welcome! I'm Erik, a product designer based in Denver, focused on creating designs that help people efficiently utilize digital products and enjoy them.
               </motion.p>
